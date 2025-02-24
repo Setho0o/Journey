fd4c18daa972416ead8b7d4ae80fe533d9f03d83
Setho0o <sethryderharris@gmail.com>
2025-02-23 20:52:16 -0600

    basic

---
+++ assets/assets.go
@@ -0,0 +1,22 @@
+package assets
+
+import "image"
+
+/*
+  planning on just getting the pixel data for the assests
+  and maping them to some id for preformance, but that means
+  we have to get the data for each indidual pixel so the bigger the tile size
+  the more time it will take to hard code it in, itll definitly be faster then
+  using the image struct on all of our cells
+*/
+
+const (
+  Tilesize = 32 
+assests = map[id]
+)
+
+
+func test(){
+  image.
+
+}
