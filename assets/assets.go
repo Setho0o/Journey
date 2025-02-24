46814ad1152a3cd5fa7a32c19734e5fbe6a93590
Setho0o <sethryderharris@gmail.com>
2025-02-23 23:53:38 -0600

    added assest support and decoders

--- assets/assets.go
+++ assets/assets.go
@@ -1,22 +1,68 @@
 package assets
 
-import "image"
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
+)
 
 /*
   planning on just getting the pixel data for the assests
-  and maping them to some id for preformance, but that means
-  we have to get the data for each indidual pixel so the bigger the tile size
-  the more time it will take to hard code it in, itll definitly be faster then
-  using the image struct on all of our cells
+  and maping them to some id for preformance
 */
+type Id int 
 
 const (
-  Tilesize = 32 
-assests = map[id]
+  LandM Id = iota
+  LandTL 
+  LandTR
+  LandBL
+  LandBR
+  
+  WaterM
+  WaterTL
+  WaterTR
+  WaterBL
+  WaterBR
+
+  TreeA
+  TreeB
+  TreeC
+  BushA
+  BushB
+  BushC
 )
 
+/*
+  could be worth it to just map the uint8 pixel instead of the rgba struct 
+  but for now we will keep it in case we need it 
+*/
 
-func test(){
-  image.
-
+func AssetInit() map[Id]*image.RGBA {
+  return map[Id]*image.RGBA {
+    LandM : DecodePng("assets/land.png"), // gonna need to organize and name the files better this was just for a test 
+  }
 }
+
+
+func DecodePng(name string) *image.RGBA {
+  file, err := os.Open(name)
+  if err != nil {
+    log.Fatal(err, "failed to open file")
+  }
+  defer file.Close()
+
+  img, err := png.Decode(file)
+  if err != nil {
+    log.Fatal(err, "failed to decode png")
+  }
+  // image.Image doesnt give us the pixels so we have to draw it to another struct 
+  rect := img.Bounds()
+  rgba := image.NewRGBA(rect)
+  draw.Draw(rgba,rect,img,rect.Min,draw.Src)
+  return rgba
+}
+
+
--- main.go
+++ main.go
@@ -1,46 +1,51 @@
 package main
 
 import (
+	"image"
 	"image/color"
+	"image/draw"
 	"log"
 	"os"
 
 	f "github.com/Setho0o/Journey/Finders"
 	gen "github.com/Setho0o/Journey/Generators"
+	a "github.com/Setho0o/Journey/assets"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 const (
-  Lx = Wx // useable pixels 
-  Ly = Wy
-  Wx = 1920 // window sizw
-  Wy = 1080
-  TileSize = 16 
+	Lx       = Wx // useable pixels
+	Ly       = Wy
+	Wx       = 1920 // window sizw
+	Wy       = 1080
+	TileSize = 16
 )
 
-
-type Game struct{
-  s *ebiten.Image
-  gen gen.Generator
-  f f.Finder
-  m Matrix
+type Game struct {
+	s   *ebiten.Image
+	gen gen.Generator
+	f   f.Finder
+	m   Matrix
+  a  map[a.Id]*image.RGBA
 }
 
-
 func (g *Game) Update() error {
-  Keys()
+	Keys()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-  g.s = screen
+	g.s = screen
 
-  for _,e := range g.m {
-    for _,e := range e {
-      g.s.Set(e.x,e.y,color.White)
-    }
+	for _, e := range g.m {
+		for _, e := range e {
+			g.s.Set(e.x, e.y, color.White)
+		}
+	}
   
-  }
+  rgba := image.NewRGBA(image.Rect(0,0,Lx,Ly)) 
+  draw.Draw(rgba, g.a[a.LandM].Bounds(), g.a[a.LandM],image.Pt(0,0),draw.Src)  
+  g.s.WritePixels(rgba.Pix) 
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -50,20 +55,21 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(Wx, Wy)
 	ebiten.SetWindowTitle("~Journey~")
-  g := GameInit()
+	g := GameInit()
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func GameInit() *Game {
-  return &Game{
-    m: MatrixInit(),
-  }
+	return &Game{
+		m: MatrixInit(),
+    a: a.AssetInit(),
+	}
 }
 
 func Keys() {
-  if ebiten.IsKeyPressed(ebiten.KeyQ) {
-    os.Exit(0)
-  }
-} 
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		os.Exit(0)
+	}
+}
